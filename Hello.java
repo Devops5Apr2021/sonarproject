a9eb8d69470c8801a0b332f41fc6078fd81e52f5
root <root@sonarmachine.us-central1-a.c.devopsproject5apr2021.internal>
2021-04-07 10:14:28 +0000

    edit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
-public class Hello {
-	public static void main(String[] args){
-		System.out.println("Hello Java method");
+package myPkg;
+public class Hello{
+// this is my comment
+// this is again a comment
+	public static void main(String[] args) {
+//		System.out.println("Hello all");
+		LOGGER.log("Hello");
 	}
+// this is final comment
 }
