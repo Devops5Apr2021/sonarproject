13f7dd5c8e19e648a10713a9617f3458064622b0
swathi-prasad <Swathi.Prasad28@yahoo.in>
2021-04-07 15:56:02 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 public static final Logger logger=null;
+	@CheckForNull
 	public static void main(String[] args) {
-	//	System.out.println("Hello all");
 		logger.log("hello");
 	}
 }
