4c5456ac34ecdddd45c5ca635502a51b87f80a89
root <root@sonarmachine.us-central1-a.c.devopsproject5apr2021.internal>
2021-04-07 10:23:17 +0000

    edit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
-public static Logger logger=null;
+public static final Logger logger=null;
 	public static void main(String[] args) {
-		System.out.println("Hello all");
+	//	System.out.println("Hello all");
 		logger.log("hello");
 	}
 }
